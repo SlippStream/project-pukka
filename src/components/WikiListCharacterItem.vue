96c0fd3a53d40f68a9bc4e137331c95a122fcd05
Nick Zimanski <nick.zimanski@gmail.com>
2023-10-22 19:42:28 -0400

    Additional formatting for aspect ratios

--- src/components/WikiListCharacterItem.vue
+++ src/components/WikiListCharacterItem.vue
@@ -3,16 +3,15 @@ defineProps(['character']);
 </script>
 
 <template>
-    <div class="wiki-character-list" :style="'background-image: linear-gradient(to right, var(--vt-c-black), rgba(100,100,100,0)), url(/src/assets/icons/characters/' + character.id + '.png);'">
-        <span class="wiki-character-list-text">{{ character.name }} -- {{ character.team }}</span>
+    <div class="wiki-character-list-item" :style="'background-image: linear-gradient(to right, var(--vt-c-black), rgba(100,100,100,0)), url(/src/assets/icons/characters/' + character.id + '.png);'">
+        <span class="wiki-character-list-item-text">{{ character.name }} -- {{ character.team }}</span>
     </div>
 </template>
 
 <style scoped>
-.wiki-character-list {
+.wiki-character-list-item {
     width: 100%;
     height: 6rem;
-    display: grid;
     background-size: 9rem;
     background-repeat: no-repeat;
     background-position: left center;
@@ -20,7 +19,7 @@ defineProps(['character']);
     border-top: 1px solid var(--vt-c-text-light-1);
     border-bottom: 1px solid var(--vt-c-text-light-1);
 }
-.wiki-character-list-text {
+.wiki-character-list-item-text {
     position: relative;
     top: 50%;
     bottom: 50%;
@@ -30,5 +29,11 @@ defineProps(['character']);
     font-weight: 500;
     margin-left: 2.5rem;
     text-shadow: 0.08em 0.08em 10px;
+}
+
+@media (min-width: 1024px) {
+    .wiki-character-list-item {
+        width: 40%;
+    }
 }
 </style>
--- src/views/CompendiumView.vue
+++ src/views/CompendiumView.vue
@@ -4,7 +4,21 @@ import CompendiumList from '../components/WikiListItem.vue';
 <style type="">
 </style>
 <template>
-    <main>
+    <div class="wiki-character-list">
         <CompendiumList/>
-    </main>
+    </div>
 </template>
+
+<style>
+.wiki-character-list {
+    width: 100%;
+    display: inline-grid;
+}
+@media (min-width: 1024px) {
+    .wiki-character-list {
+        grid-template-columns: auto auto;
+        column-gap: 2rem;
+        justify-content: space-evenly;
+    }
+}
+</style>
